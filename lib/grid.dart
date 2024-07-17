5680bf702d0e47783e477ff2a741a0699cf96846
Lynnn01 <jetsadathegame@gmail.com>
2024-07-17 11:42:37 +0700

    grid viwe

--- lib/grid.dart
+++ lib/grid.dart
@@ -13,10 +13,33 @@ class _GridPageState extends State<GridPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Grid Viwe"),
+        scrolledUnderElevation: 0,
+        title: const Text("Grid View"),
       ),
       drawer: const MyDrawer(),
-      body: Container(),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // จำนวนคอลัมน์ในกริด
+          childAspectRatio: 1.0, // ทำให้แต่ละไอเท็มเป็นสี่เหลี่ยมจัตุรัส
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.all(4.0), // ระยะห่างรอบไอเท็ม
+            decoration: BoxDecoration(
+              color: Colors.white, // สีพื้นหลังของไอเท็ม
+              border: Border.all(
+                color: Colors.black, // สีกรอบของไอเท็ม
+                width: 2.0, // ความหนาของกรอบ
+              ),
+              borderRadius: BorderRadius.circular(8.0), // ความโค้งของมุมกรอบ
+            ),
+            child: Center(
+              child: Text("Item $index"),
+            ),
+          );
+        },
+        itemCount: 20, // จำนวนไอเท็มในกริด
+      ),
     );
   }
 }
